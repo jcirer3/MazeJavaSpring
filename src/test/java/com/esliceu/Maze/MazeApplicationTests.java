48376a3aa28c4ba3dca928894409992aadfc219d
jcirer3 <juancier9@gmail.com>
2024-11-13 15:53:26 +0100

    commensant

---
+++ src/test/java/com/esliceu/Maze/MazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esliceu.Maze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
